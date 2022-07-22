bcd8d8d1fc062846f6b0bb2547c6a9a1dee466b8
raki <mohammed.raki26@gmail.com>
2022-07-22 17:32:18 +0200

    modification boutique

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,21 @@
 package test;
 
+import metier.Boutique;
+import metier.Clien;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Boutique b1 = new Boutique("Louis Vuitton","Avenue montaigne");
+		Boutique b2 = new Boutique("Snipes","Avenue De la marche");
+		
+		Clien c1 = new Clien("mohammed","raki");
+		Clien c2 = new Clien("mohammed1","raki1");
+		
+		
+		
 
 	}
 
